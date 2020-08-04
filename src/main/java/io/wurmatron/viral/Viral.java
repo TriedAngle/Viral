3c03a6dc88b0cd33c72575a11569ca53ba3c39dd
Sebastian <Sebastian@Strobl.net>
2020-08-05 01:03:21 +0200

    Update:  "minimal setup"

--- src/main/java/io/wurmatron/viral/Viral.java
+++ src/main/java/io/wurmatron/viral/Viral.java
@@ -1,48 +1,38 @@
 package io.wurmatron.viral;
-
-import io.wurmatron.viral.api.capabilities.IViral;
-import io.wurmatron.viral.api.capabilities.ViralFactory;
-import io.wurmatron.viral.api.capabilities.ViralStorage;
-import io.wurmatron.viral.common.config.ConfigHolder;
-import io.wurmatron.viral.common.event.ViralEvents;
 import io.wurmatron.viral.common.references.Global;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.VersionChecker;
-import net.minecraftforge.fml.VersionChecker.CheckResult;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(value = Global.MODID)
+@Mod(Global.MODID)
 public class Viral {
 
-  public static final Logger LOGGER = LogManager.getLogger();
-  public static CheckResult versionStatus;
+
+  public static final Logger LOGGER = LogManager.getLogger(Global.MODID);
 
   public Viral() {
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-    // Config
-    ModLoadingContext loadContext = ModLoadingContext.get();
-    loadContext.registerConfig(Type.CLIENT, ConfigHolder.CLIENT_SPEC);
-    loadContext.registerConfig(Type.COMMON, ConfigHolder.COMMON_SPEC);
+
+    MinecraftForge.EVENT_BUS.register(this);
   }
 
   private void setup(FMLCommonSetupEvent event) {
-    versionStatus = VersionChecker
-        .getResult(ModLoadingContext.get().getActiveContainer().getModInfo());
-    CapabilityManager.INSTANCE
-        .register(IViral.class, new ViralStorage(), new ViralFactory());
-    MinecraftForge.EVENT_BUS.register(new ViralEvents());
+    LOGGER.info("Setting up Viral");
   }
 
   private void doClientStuff(FMLClientSetupEvent event) {
 
+  }
+
+  @SubscribeEvent
+  public void onServerStarting(FMLServerStartedEvent event) {
+    LOGGER.info("Hello from Viral");
   }
 }
